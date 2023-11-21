0197a5d15c244f24376b4b739698e30da6ae0c3c
Muriel Gordon <murielgordon6@gmail.com>
2023-11-20 16:47:19 -0800

    re-nested if statements to make blank action statement work

--- app.py
+++ app.py
@@ -28,21 +28,21 @@ def game():
             room = planisphere.load_room(room_name)
             next_room = room.go(action)
 
-        if not next_room and action == '':
-            session['room_name'] = planisphere.name_room(room)
+            if not next_room and action == '':
+                session['room_name'] = planisphere.name_room(room)
 
-        #elif not next_room and room_name == 'laser_weapon_armory' and session['count'] < 9:
-        #    session['count'] += 1
-            #stay = (request.form.get('action') != '666')
-        #    next_room = room.go('stay')
-        #    session['room_name'] = planisphere.name_room(next_room)
+            #elif not next_room and room_name == 'laser_weapon_armory' and session['count'] < 9:
+            #    session['count'] += 1
+                #stay = (request.form.get('action') != '666')
+            #    next_room = room.go('stay')
+            #    session['room_name'] = planisphere.name_room(next_room)
 
-        elif not next_room:
-            next_room = room.go('*')
-            session['room_name'] = planisphere.name_room(next_room)    
+            elif not next_room:
+                next_room = room.go('*')
+                session['room_name'] = planisphere.name_room(next_room)    
 
-        else:
-            session['room_name'] = planisphere.name_room(next_room)
+            else:
+                session['room_name'] = planisphere.name_room(next_room)
         
     return redirect(url_for("game"))    
     
